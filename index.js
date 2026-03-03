fe28e881c18eea05b543f6de22e48aa036294bad
Velan D <velan.d@amadeus.com>
2026-03-03 19:54:09 +0530

    Added some styles

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Replace the parameters below according to your preference.
 
 // space, scary, military, romantic, cowboy, fantasy, superhero
-favouriteMovieGenre("regular")
+favouriteMovieGenre("space")
 
 // watermelon, tomato, banana, orange, avocado, blueberry
 favouriteFruit("regular")
@@ -11,7 +11,7 @@ favouriteFruit("regular")
 favouriteMode("light")
 
 // sharp, soft, round
-favouriteEdgeStyle("sharp")
+favouriteEdgeStyle("soft")
 
 
 
